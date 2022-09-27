6ae6e13fcd9d9168221110151f72af11cf5a42c3
Razmoth <32140579+Razmoth@users.noreply.github.com>
2022-09-27 17:50:37 +0400

    Add project files.

---
+++ Hololive/Program.cs
@@ -0,0 +1,32 @@
+﻿using System.Text;
+
+namespace Hololive
+{
+    public class Program
+    {
+        public static void Main(string[] args)
+        {
+            if (args.Length != 3)
+            {
+                Help();
+                return;
+            }
+            var input = new FileInfo(args[0]);
+            var output = new FileInfo(args[1]);
+            var key = args[2];
+            Decryptor.DecryptBundle(input.FullName, output.FullName, key);
+        }
+
+        public static void Help()
+        {
+            Console.OutputEncoding = Encoding.UTF8;
+            Console.WriteLine("Usage: Hololive <input> <output> <key>\n");
+            Console.WriteLine("<input>\tInput file.");
+            Console.WriteLine("<output>\tOutput Folder.");
+            Console.WriteLine("<key>\tAES ECB 256 decrytion key.\n");
+            Console.WriteLine("Known Keys:");
+            Console.WriteLine("hololive ERROR Demo: マgとNにAｾAｼcnヒﾙチねﾓヨモしjゆムアAIiソみiコ");
+            Console.WriteLine("hololive ERROR the Game: イくZuwツらﾊなよ7ｹKJむやQHなヌさK4");
+        }
+    }
+}
---
+++ README.md
@@ -0,0 +1,15 @@
+# Hololive
+Tool to decrypt hololive games.
+
+How to use:
+```
+Usage: Hololive <input> <output> <key>
+
+<input> Input file.
+<output>        Output Folder.
+<key>   AES ECB 256 decrytion key.
+
+Known Keys:
+hololive ERROR Demo: マgとNにAｾAｼcnヒﾙチねﾓヨモしjゆムアAIiソみiコ
+hololive ERROR the Game: イくZuwツらﾊなよ7ｹKJむやQHなヌさK4
+```
